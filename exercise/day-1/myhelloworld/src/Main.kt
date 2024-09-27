cf254909189b7c0520b9e3ece19df0524c56aa10
ZKartadimaja <zulfikarzamzam13@gmail.com>
2024-09-27 14:47:49 +0700

    story-1 excercise

--- exercise/day-1/myhelloworld/src/Main.kt
+++ exercise/day-1/myhelloworld/src/Main.kt
@@ -1,4 +1,26 @@
+import java.util.Scanner
+
 fun main() {
     val name = "Hello World"
     println(name+ "!")
+
+    var score: Int = 78
+    if (score > 90 && score <= 100) {
+        println("A")
+    }
+    else if (score >= 80 && score <= 89) {
+        println("B")
+    }
+    else if (score >= 70 && score <= 79) {
+        println("Your Grade is C")
+    }
+    else if (score >= 60 && score <= 69) {
+        println("Your Grade is D")
+    }
+    else if (score >= 0 && score <= 60) {
+        println("Your Grade is F")
+    }
+    else {
+        println("Please input number from 0 - 100")
+    }
 }
